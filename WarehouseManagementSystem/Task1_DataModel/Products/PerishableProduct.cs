a838eeaac40662d7d0a179523b1f472086c5acc7
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-01-19 21:25:19 +0200

    made PermishableProduct class

--- WarehouseManagementSystem/Task1_DataModel/Products/PerishableProduct.cs
+++ WarehouseManagementSystem/Task1_DataModel/Products/PerishableProduct.cs
@@ -8,4 +8,29 @@ namespace WarehouseManagementSystem.Task1_DataModel.Products;
 
 public class PerishableProduct : Product
 {
+    public DateTime expiryDate{ get; private set; }
+
+    public PerishableProduct(DateTime expiryDate,
+        string name,
+        double price,
+        double weight,
+        int quantity) : base(name, price, weight, quantity)
+    {
+        this.expiryDate = expiryDate;
+    }
+
+    public bool IsExpired()
+    {
+        return DateTime.Now.Date > expiryDate;
+    }
+
+    public override void GetStorageRequirements()
+    {
+        Console.WriteLine("Store in cold place");
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.name}, Price: {this.basePrice}, Weight: {this.weight}, Quantity: {this.quantity}";
+    }
 }
