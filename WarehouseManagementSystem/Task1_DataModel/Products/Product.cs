9e8fc489289def63f331f6ac11b2d7beffd85df1
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-01-19 21:14:12 +0200

    made class product

--- WarehouseManagementSystem/Task1_DataModel/Products/Product.cs
+++ WarehouseManagementSystem/Task1_DataModel/Products/Product.cs
@@ -8,4 +8,21 @@ namespace WarehouseManagementSystem.Task1_DataModel.Products;
 
 public abstract class Product
 {
+    private static int _id = 0;
+    
+    public int id { get; private set; }
+    public string name { get; private set; }
+    public double basePrice { get; protected set; }
+    public double weight { get; protected set; }
+    public int quantity { get; protected set; }
+
+    public Product(string name, double basePrice, double weight, int quantity)
+    {
+        id = _id++;
+        this.name = name;
+        this.basePrice = basePrice;
+        this.weight = weight;
+        this.quantity = quantity;
+    }
+    public abstract void GetStorageRequirements();
 }
