4f72d6444b0da1774671ee314dbc1ad368514d1e
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-01-19 21:36:35 +0200

    made electronic class

--- WarehouseManagementSystem/Task1_DataModel/Products/Electronics.cs
+++ WarehouseManagementSystem/Task1_DataModel/Products/Electronics.cs
@@ -8,4 +8,27 @@ namespace WarehouseManagementSystem.Task1_DataModel.Products;
 
 public class Electronics : Product
 {
+    public DateTime warrantyPeriod { get; private set; }
+    public double voltage { get; private set; }
+
+    public Electronics(DateTime warrantyPeriod,
+        double voltage,
+        string name,
+        double basePrice,
+        double weight,
+        int quantity) : base(name, basePrice, weight, quantity)
+    {
+        this.warrantyPeriod = warrantyPeriod;
+        this.voltage = voltage;
+    }
+
+    public bool IsWarrantyPeriodValid()
+    {
+        return DateTime.Now.Date <  this.warrantyPeriod.Date;
+    }
+
+    public override void GetStorageRequirements()
+    {
+        Console.WriteLine($"Store in a dry place.");
+    }
 }
